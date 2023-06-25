416fc52e04cacef90dba87096c6a66b6bb8fc96e
Dane Chan <114413054+TheBanana03@users.noreply.github.com>
2023-06-26 03:12:13 +0800

    never gonna give you up
    never gonna let you down
    never gonna run around
    and desert you

---
+++ js/like.js
@@ -0,0 +1,17 @@
+
+function likeThis() {
+    
+    var like = document.getElementById("like");
+    like.classList.toggle("liked");
+}
+
+function likeThis2() {
+    
+    var like2 = document.getElementById("like2");
+    like2.classList.toggle("liked");
+}
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
