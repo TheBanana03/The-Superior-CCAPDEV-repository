9e6ce8712a9bb5d08dfdcaa3ba3cec1dce20b15b
Dominic Sia <siadominic@gmail.com>
2023-07-23 17:17:53 +0800

    test

--- routes/community.js
+++ routes/community.js
@@ -5,20 +5,19 @@ const { mongooseToObj, multipleMongooseToObj } = require('../models/db');
 
 const router = express.Router();
 
-const multerConfig = {
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, path.join(__dirname, '../public/assets/community'));
-        },
-        filename: (req, file, cb) => {
-            cb(null, Date.now() + "_" + file.originalname);
-        }
-    }),
-};
-
 //Go to Home Page 
 router.get('/', (req, res) => {
     res.redirect('/');
+});
+
+//Go to Create Community Page
+router.get('/create', (req, res) => {
+    const user = req.session.user;
+    
+    res.render('createcommunity', {
+        title: 'Animo Create Community',
+        user: user
+    });
 });
 
 //Go to Community Page
@@ -27,13 +26,14 @@ router.get('/:name', async (req, res) => {
   
     try {
         const community = mongooseToObj(await Community.findOne({ name }));
-        const community_follower_count = await User.countDocuments({ followed_communities: community._id });
   
         if (!community) {
             return res.render('404', {
                 title: '404'
             });
         }
+
+        const community_follower_count = await User.countDocuments({ followed_communities: community._id });
   
         res.render('community', {
             title: `${community.name}`,
@@ -42,11 +42,17 @@ router.get('/:name', async (req, res) => {
             community_follower_count: community_follower_count
         });
 
-        } catch (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        }
-  });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+//Go to Edit Community Page
+router.get('/:name/edit', async (req, res) => {
+    const name = req.params.name;
+    const user = req.session.user;
+});
 
 //Create Community
 router.post('/', async (req, res) => {
@@ -75,24 +81,6 @@ router.post('/', async (req, res) => {
             error: 'Error creating community\n' + err
         });
     }
-});
-
-//Go to Create Community Page
-router.get('/create', (req, res) => {
-    const user = req.session.user;
-    
-    res.render('createcommunity', {
-        title: 'Animo Create Community',
-        user: user
-    });
-});
-
-
-//Go to Edit Community Page
-router.get('/:name/edit', async (req, res) => {
-    const name = req.params.name;
-    const user = req.session.user;
-
 });
 
 module.exports = router;
