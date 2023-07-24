be5f31280b258c634311ed8b507b0d0a7c16cd07
nicoledaphneong <nicole_daphne_ong@dlsu.edu.ph>
2023-07-24 16:05:47 +0800

    Search GUI and navbar

--- routes/search.js
+++ routes/search.js
@@ -20,6 +20,7 @@ router.post('/', async (req, res) => {
         ],
       })
       .populate('creator', 'username profilePicturePath') // Populate the "creator" field with the specified properties
+      .populate('community', 'name') // Populate the "community" field with the "name" property
       .populate('likes', 'username') // Populate the "likes" field with the specified properties
       .lean(); // Convert the Mongoose documents to plain JavaScript objects
   
