6ce7184a367651113a2c29da615c9f9c6bdd5337
Dane Chan <114413054+TheBanana03@users.noreply.github.com>
2023-08-06 01:04:57 +0800

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
-const { comparePassword } = require('./hashPassword');
+const { comparePassword } = require('../hashPassword');
 
 const router = express.Router();
 
