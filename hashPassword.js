baf60637ae5473a257b221a9d93fe174e7b41098
Dane Chan <114413054+TheBanana03@users.noreply.github.com>
2023-08-06 01:03:25 +0800

    Add files via upload

---
+++ hashPassword.js
@@ -0,0 +1,14 @@
+// helpers.js
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+    hashPassword: async function (password) {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return hashedPassword;
+    },
+    comparePassword: async function (password1, password2) {
+        const comparePassword = await bcrypt.compare(password1, password2);
+        return comparePassword;
+    }
+};
