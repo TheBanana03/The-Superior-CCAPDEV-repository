395c709896eeac46398fd356ae42635a77c7a86f
nicoledaphneong <nicole_daphne_ong@dlsu.edu.ph>
2023-08-06 17:08:01 +0800

    Username, email, password input validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     const usernameInput = document.querySelector("#username");
     const usernameError = document.querySelector("#usernameError");
+    const emailInput = document.querySelector("#email");
+    const confirmEmailInput = document.querySelector("#confirm-email");
+    const passwordInput = document.querySelector("#password");
+    const confirmPasswordInput = document.querySelector("#confirm-password");
     const submitButton = document.querySelector("#submitButton");
+    const checkUsernameAction = document.querySelector("#check-username-action");
+
+    let isUsernameInvalid = false;
+    let isUsernameTaken = false;
 
     async function checkUsernameAvailability(username) {
-        console.log('Checking username availability:', username); // Add this line
+        console.log('Checking username availability:', username);
         try {
             const response = await fetch(`/signup/checkUsername?username=${username}`);
             if (response.ok) {
                 const data = await response.json();
-                console.log('Username availability response:', data); // Add this line
+                console.log('Username availability response:', data);
                 return data.exists;
             } else {
                 console.error("An error occurred while checking username");
@@ -19,14 +27,16 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("An error occurred while checking username:", error);
             return false;
         }
-    }  
+    }
 
     function validateUsername(username) {
         if (username.length < 3 || username.length > 16) {
             usernameError.textContent = "Username must be between 3 and 16 characters long.";
+            isUsernameInvalid = true;
             return false;
         } else {
             usernameError.textContent = "";
+            isUsernameInvalid = false;
             return true;
         }
     }
@@ -38,8 +48,9 @@ document.addEventListener("DOMContentLoaded", function () {
             const exists = await checkUsernameAvailability(username);
             if (exists) {
                 usernameError.textContent = "Username already exists. Please choose another one.";
+                isUsernameTaken = true;
                 submitButton.disabled = true;
-            } else {
+            } else if (!isUsernameTaken) {
                 usernameError.textContent = "";
                 submitButton.disabled = false;
             }
@@ -52,4 +63,54 @@ document.addEventListener("DOMContentLoaded", function () {
         const username = usernameInput.value;
         updateUsernameStatus(username);
     });
+
+    confirmEmailInput.addEventListener("input", updateSubmitButton);
+    confirmPasswordInput.addEventListener("input", updateSubmitButton);
+    
+    function validateEmail(email, confirmEmail) {
+        if (email !== confirmEmail) {
+            emailInput.setCustomValidity("Emails do not match");
+            confirmEmailInput.setCustomValidity("Emails do not match");
+            document.querySelector("#emailError").textContent = "Emails do not match";
+            return false;
+        } else {
+            emailInput.setCustomValidity("");
+            confirmEmailInput.setCustomValidity("");
+            document.querySelector("#emailError").textContent = "";
+            return true;
+        }
+    }
+
+    function validatePassword(password, confirmPassword) {
+        if (password !== confirmPassword) {
+            passwordInput.setCustomValidity("Passwords do not match");
+            confirmPasswordInput.setCustomValidity("Passwords do not match");
+            document.querySelector("#passwordError").textContent = "Passwords do not match";
+            return false;
+        } else {
+            passwordInput.setCustomValidity("");
+            confirmPasswordInput.setCustomValidity("");
+            document.querySelector("#passwordError").textContent = "";
+            return true;
+        }
+    }
+
+    function updateSubmitButton() {
+        const email = emailInput.value;
+        const confirmEmail = confirmEmailInput.value;
+        const password = passwordInput.value;
+        const confirmPassword = confirmPasswordInput.value;
+
+        const isEmailValid = validateEmail(email, confirmEmail);
+        const isPasswordValid = validatePassword(password, confirmPassword);
+
+        submitButton.disabled = !(isUsernameInvalid || isUsernameTaken) || !isEmailValid || !isPasswordValid;
+    }
+
+    usernameInput.addEventListener("input", updateSubmitButton);
+    confirmEmailInput.addEventListener("input", updateSubmitButton);
+    confirmPasswordInput.addEventListener("input", updateSubmitButton);
+
+    updateSubmitButton();
+
 });
