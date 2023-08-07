dadc641eb0f0f7486980ffa837461a7651dea97c
Dominic Sia <siadominic@gmail.com>
2023-08-08 00:28:53 +0800

    .

--- public/js/scripts2.js
+++ public/js/scripts2.js
@@ -19,7 +19,7 @@ function fetchNestedComments(postId, commentId) {
     return new Promise((resolve, reject) => {
         fetch(apiUrl)
             .then(response => {
-                // console.log(response);
+                console.log(response);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
